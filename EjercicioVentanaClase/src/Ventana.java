e11462237eff473bd3808fa592964d0d432ae33c
ivang <ivang@10.164.53.48>
2019-09-24 08:27:48 +0200

    Ventanita

--- EjercicioVentanaClase/src/Ventana.java
+++ EjercicioVentanaClase/src/Ventana.java
@@ -1,10 +1,23 @@
+import javax.swing.JFrame;
 
-public class Ventana {
+public class Ventana extends JFrame{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Ventana() {
+		this.setTitle("Mi primera Ventana");
+		this.setSize(320,240);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
+		System.out.println("ASASSAASSA");
+		Ventana v1 = new Ventana();
+		v1.setVisible(true);
 
 	}
 
